10537e12233256f24f651be86fc566520d0fe553
Alexander Kulakov <a.kulakov@mail.ru>
2013-12-02 10:31:41 -0400

    Switch API to JSON-encoded responses

--- keen/web/urls.py
+++ keen/web/urls.py
@@ -1,14 +1,18 @@
 from django.conf.urls import patterns, url, include
 
-from .views import client
-from .views.api.client import CustomerList, CustomerProfile
+from .views.api.client import ClientProfile, CustomerList, CustomerProfile
 
 
 api_urls = patterns(
-    'keen.web.views.api',
-    url(r'^customers/$', CustomerList.as_view(), name='api_customer_list'),
-    url(r'^customers/(?P<offset>\d+)$', CustomerList.as_view(), name='api_customer_list'),
-    url(r'^customers/(?P<customer_id>\d+)$', CustomerProfile.as_view()),
+    '',
+    url(r'^client/(?P<client_slug>[\w-]+)$', ClientProfile.as_view(),
+        name='client_profile'),
+    url(r'^client/(?P<client_slug>[\w-]+)/(?P<part>summary)$',
+        ClientProfile.as_view(), name='client_profile'),
+    url(r'^client/(?P<client_slug>[\w-]+)/customers$', CustomerList.as_view(),
+        name='api_customer_list'),
+    url(r'^client/(?P<client_slug>[\w-]+)/customer/(?P<customer_id>\d+)$',
+        CustomerProfile.as_view(), name='api_customer_profile'),
 )
 
 
@@ -20,5 +24,5 @@ urlpatterns = patterns(
     url(r'^client/profile$', 'client.profile', name='client_profile'),
     url(r'^client/customer_form$', 'client.customer_form', name='customer_form'),
     url(r'^client/signup-form/create$', 'client.signup_form_create', name='signup_form_create'),
-    url(r'^client/api/', include(api_urls)),
+    url(r'^api/', include(api_urls)),
 )
