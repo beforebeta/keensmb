0450d695ae1fa6d3fc32f9e27628a0e4d31dc8dd
Alexander Kulakov <a.kulakov@mail.ru>
2013-12-09 12:15:34 -0400

    Add missing assets

---
+++ keen/web/static/js/profiles.js
@@ -0,0 +1,45 @@
+// JS for profiles page
+'use strict';
+
+(function($) {
+
+    var checkItemActions = function() {
+        var $customersTable = $('#customers-table'),
+            checkboxes = $customersTable.find(':checkbox'),
+            checkedAny = (checkboxes.filter(':checked').length !== 0);
+
+        $('.js-item-selected')[checkedAny ? 'slideDown' : 'slideUp'](200);
+    };
+
+    var deleteCustomer = function() {
+
+        // do ajax call here, with callback:
+
+        $('.js-customer-deleted-name').text('John Smith');
+        $('.customer-deleted-alert').show().addClass('in');
+    };
+
+    var closeGlobalAlert = function(e) {
+        e.preventDefault();
+        var $alertBlock = $(this).closest('.global-alert');
+        $alertBlock.removeClass('in').hide();
+    };
+
+    $('#new-field-query').typeahead({
+        source: function (query, process) {
+            return ['opop', 'apap', 'ipip', 'opasd', 'asdfgh', 'jklpoiu', 'ytrewq', 'zxccvbn'];
+            // return $.get('/profile/custom-fields', { query: query }, function (data) {
+            //     return process(data.options);
+            // });
+        }
+    });
+
+
+
+    // Table: Add class row selected
+    $(document).on('toggle', '#customers-table :checkbox', checkItemActions);
+
+    $(document).on('click', '.js-delete-customer', deleteCustomer);
+    $(document).on('click', '.global-alert .close', closeGlobalAlert);
+
+})(jQuery);
