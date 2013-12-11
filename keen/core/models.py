5fd9656fe8157e1f653b2407ba9e441497ef617f
Alexander Kulakov <a.kulakov@mail.ru>
2013-12-11 11:53:11 -0400

    Remove hardcoded field names

--- keen/core/models.py
+++ keen/core/models.py
@@ -252,12 +252,7 @@ class Customer(Timestamps):
             return "/static/images/icons/dude.svg"
 
     def get_name(self):
-        first_name = self._return_field(CUSTOMER_FIELD_NAMES.first_name)
-        last_name = self._return_field(CUSTOMER_FIELD_NAMES.last_name)
-        if first_name or last_name:
-            return "%s %s" % (first_name, last_name)
-        else:
-            return ""
+        return  self.data.get(CUSTOMER_FIELD_NAMES.full_name, '')
     get_name.short_description = 'Name'
 
     def get_email(self):
