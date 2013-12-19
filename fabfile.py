6049848158f95ace067510594f863ebc67717552
Alexander Kulakov <a.kulakov@mail.ru>
2013-12-19 09:06:03 -0400

    Improve deploy command

--- fabfile.py
+++ fabfile.py
@@ -6,6 +6,7 @@ defaults if you are happy with the default layout.
 """
 from fabric.api import (
     run,
+    sudo,
     local,
     env,
     settings,
@@ -36,20 +37,6 @@ def install_os_packages():
     sudo('aptitude install git nginx build-essential python-dev postgresql libpq-dev libffi-dev python-virtualenv')
 
 
-def prepare_db():
-    sudo('service postgresql start')
-
-    with prefix('sudo -u postfix -i'):
-        run('createuser -S -D -R keen')
-        run('createdb -O keen -T template0 -E utf8 keen')
-        run('psql -c "create extension hstore;" keen')
-
-    with virtualenv():
-        run('./manage.py syncdb')
-        run('./manage.py migrate')
-        run('./manage.py setup --all')
-
-
 def install_dependencies():
     assert 'profile' in env
 
@@ -70,7 +57,7 @@ def clone(repo=None):
     run('git clone %s %s' % (repo, env.project_dir))
 
     local_py = 'keen/settings/local.py'
-    put(local_py, '%(project_dir)s/settings/')
+    put(local_py, '%(project_dir)s/keen/settings/' % env)
 
 
 def pull(branch=''):
@@ -87,6 +74,39 @@ def checkout(branch):
 def migrate():
     with virtualenv():
         run('./manage.py migrate --merge')
+
+
+@task
+def drop_db():
+    """Destroy database and datanase user
+    """
+    with warn_only():
+        sudo('dropdb keen', user='postgres')
+        sudo('dropuser keen', user='postgres')
+
+
+@task
+def create_db():
+    """Create database, database user and HSTORE extension then create/migrate
+    schema using South
+    """
+    sudo('service postgresql start')
+
+    with warn_only():
+        sudo('createuser -S -D -R keen', user='postgres')
+        sudo('createdb -O keen -T template0 -E utf8 keen', user='postgres')
+        sudo('psql -c "create extension hstore;" keen', user='postgres')
+
+    with virtualenv():
+        run('./manage.py syncdb --noinput')
+
+
+@task
+def sample_data():
+    """Populate database with sample data
+    """
+    with virtualenv():
+        run('./manage.py setup --all')
 
 
 @task
@@ -145,10 +165,18 @@ def nginx_restart():
 
 
 @task
-def deploy(profile, branch, setup=None):
-    """Deploy current branch or branch specified as argument
+def deploy(profile, branch):
+    """Deploy specified branch
 
-    Target host must be specified by using either "staging" or "production" command
+    Profile can be either "development" or "production"
+    Branch is branch nage as used by Git (ex. origin/feature-1)
+
+    This command will do the following steps:
+        * checkout branch
+        * install required Python packages usin pip
+        * migrate database using South
+        * run "manage.py collectstatic"
+        * restart uwsgi daemon
     """
     assert exists(env.project_dir), '''
     It seems that you did not do install on that host.
@@ -160,10 +188,10 @@ def deploy(profile, branch, setup=None):
     assert branch, "Please specify branch to checkout on that host"
     checkout(branch)
 
+    install_dependencies()
+
     migrate()
     with virtualenv():
-        if setup:
-            run('./manage.py setup --all')
         run('./manage.py collectstatic --noinput')
     with warn_only():
         uwsgi_stop()
@@ -174,43 +202,45 @@ def deploy(profile, branch, setup=None):
 
 
 @task
-def install(repo):
+def install(profile, repo):
     """Initial installation
+
+    Profile must be either "development" or "production"
+    Repo is URL of Git repository to clone (ex. git@github.com:beforebeta/keensmb.git)
 
     That includes the following steps
 
-        1. Install Ubuntu packages
-        2. Create virtualenv
-        3. Create database and database user
-        4. Clone git repository (accepts optional git repository URL,
+        * Install Ubuntu packages
+        * Create virtualenv
+        * Clone git repository (accepts optional git repository URL,
                     default is git@github.com:beforebeta/keensmb.git)
-        5. Install Python packages listed in requirements file
-        6. Add keensmb.com virtual host to Nginx configuration
-
-    Note:
-        Target host must be specified by using either "staging" or "production"
-        command as following:
-            fab staging:test install:git@github.com:momyc/keensmb.git
-            or
-            fab production:keensmb.com install
+        * Install Python packages listed in requirements file
+        * Create database and database user
+        * Add keensmb.com virtual host to Nginx configuration
+        * Create media directory
     """
     assert not exists(env.virtualenv), '''
     It seems that installation has been already done on that host.
     Please consider "deploy" command instead OR manually remove %(virtualenv)s directory
     ''' % env
 
+    assert profile in ('development', 'production')
+    env.profile = profile
+
     install_os_packages()
     run("virtualenv --no-site-packages %(virtualenv)s" % env)
-    prepare_db()
     clone(repo)
     install_dependencies()
+    create_db()
     configure_nginx()
+    with cd(env.project_dir):
+        sudo('mkdir media')
+        sudo('chown www-data:www-data media')
+
     print """
     Installation is complete. Please make following changes manually:
 
         * Edit PostgreSQL pg_hba.conf to allow user keen to connect to database
         keen through UNIX-socket without password. This file is located at /etx/postgresql/
-        or similar directory. Insert the following line into it:
-
-            local   keen    keen    trust
+        or similar directory.
     """
