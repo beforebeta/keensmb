dcfe01cb30af3d8ab573daa71a63bd1e7af9e304
Sergii Iushchuk <edifear@gmail.com>
2013-12-21 13:24:35 +0200

    signup edit page integrating

--- keen/web/static/js/sign-up.js
+++ keen/web/static/js/sign-up.js
@@ -12,12 +12,12 @@
         //     console.log(data);
         // });
 
-        // Initial data:
-
-        $scope.title = {text: 'some title', isEditing: false};
-        $scope.permalink = {text: 'some-perma-link', isEditing: false};
-        $scope.formTitle = {text: 'Header 4 would have max 75 characters', isEditing: false, height: 70};
-        $scope.formDescription = {text: 'Description would have max 75 characters', isEditing: false, height: 70};
+        suService.getInitialData().then(function(data) {
+            $scope = angular.extend($scope, parseFormData(data));
+            $timeout(function() {
+                initDraggableImages();
+            }, 300);
+        });
 
         $scope.globalAlert = false;
         var notify = function(text) {
@@ -54,6 +54,11 @@
         };
 
         $scope.validateSlug = function(slug) {
+            if (!$scope.formType) {
+                $scope.saveEditing(slug);
+                return true;
+            }
+
             suService.checkFormSlug(slug.text).then(function(res) {
                 // alert('Slug: "'+slug.text+'" already exists');
                 notify('Slug: "'+slug.text+'" already exists');
@@ -83,7 +88,7 @@
         $scope.cancelEditingBanner = function() {
             $scope.bannerLogo = angular.copy(lastBannerLogo);
             stopEditingBanner(lastBannerLogo);
-            $('#banner-preview').css({'left': $scope.bannerLogo.image.left, 'top': $scope.bannerLogo.image.top});
+            $bannerLogo.css({'left': $scope.bannerLogo.image.left, 'top': $scope.bannerLogo.image.top});
         };
 
         $scope.startRepositionBanner = function() {
@@ -131,7 +136,7 @@
         $scope.cancelEditingBgImage = function() {
             $scope.backgroundImage = angular.copy(lastBackgroundImage);
             stopEditingBgImage(lastBackgroundImage);
-            $('#banner-preview').css({'left': $scope.backgroundImage.image.left, 'top': $scope.backgroundImage.image.top});
+            $backgroundImage.css({'left': $scope.backgroundImage.image.left, 'top': $scope.backgroundImage.image.top});
         };
 
         $scope.startRepositionBgImage = function() {
@@ -248,7 +253,10 @@
         };
 
         $scope.saveForm = function() {
-
+            if (!$scope.formType) {
+                saveFormData();
+                return true;
+            }
             // validate slug
             suService.checkFormSlug($scope.permalink.text).then(function(res) {
                 notify('Slug: "'+ $scope.permalink.text+ '" already exists');
@@ -268,6 +276,43 @@
                     saveFormAsPreviewData();
                 });
             }, 100);
+        };
+
+        var parseFormData = function(formData) {
+            var prepared = {};
+            prepared.title = {text: formData.pageTitle, isEditing: false};
+            prepared.permalink = {text: formData.permalink, isEditing: false};
+            prepared.bannerLogo = {
+                image: {
+                    src: formData.bannerLogo.imageSrc,
+                    top: formData.bannerLogo.position.top,
+                    left: formData.bannerLogo.position.left
+                },
+                uploaded: !!formData.bannerLogo.imageSrc
+            };
+            prepared.backgroundImage = {
+                image: {
+                    src: formData.backgroundImage.imageSrc,
+                    top: formData.backgroundImage.position.top,
+                    left: formData.backgroundImage.position.left
+                },
+                uploaded: !!formData.backgroundImage.imageSrc,
+                backgroundColor: formData.backgroundImage.backgroundColor
+            };
+            prepared.form = {
+                backgroundColor: formData.form.backgroundColor,
+                textColor: formData.form.textColor
+            };
+            prepared.formTitle = {
+                text: formData.form.title,
+                height: formData.form.titleHeight
+            };
+            prepared.formDescription = {
+                text: formData.form.description,
+                height: formData.form.descriptionHeight
+            };
+
+            return prepared;
         };
 
         var prepareFormData = function() {
@@ -303,26 +348,33 @@
                     descriptionHeight: $scope.formDescription.height
                 }
             };
-            console.log(formData.form);
 
+            // save images to server
             var imagesData = [];
-
             if ($scope.bannerLogo.image.src !== defaultImageSrc) {
-                var bannerLogoImgData = suService.uploadClientImage(
-                        cleanBase64($scope.bannerLogo.image.src), // img base64 data
-                        $scope.bannerLogo.image.type,             // img type
-                        'banner'                                  // img target
-                    );
-                imagesData.push(bannerLogoImgData);
+                if (!$scope.bannerLogo.image.type) {
+                    formData.bannerLogo.imageSrc = $scope.bannerLogo.image.src;
+                } else {
+                    var bannerLogoImgData = suService.uploadClientImage(
+                            cleanBase64($scope.bannerLogo.image.src), // img base64 data
+                            $scope.bannerLogo.image.type,             // img type
+                            'banner'                                  // img target
+                        );
+                    imagesData.push(bannerLogoImgData);
+                }
             }
 
             if ($scope.backgroundImage.image.src !== defaultImageSrc) {
-                var backgroundImageData = suService.uploadClientImage(
-                        cleanBase64($scope.backgroundImage.image.src), // img base64 data
-                        $scope.backgroundImage.image.type,             // img type
-                        'background'                                   // img target
-                    );
-                imagesData.push(backgroundImageData);
+                if (!$scope.backgroundImage.image.type) {
+                    formData.backgroundImage.imageSrc = $scope.backgroundImage.image.src;
+                } else {
+                    var backgroundImageData = suService.uploadClientImage(
+                            cleanBase64($scope.backgroundImage.image.src), // img base64 data
+                            $scope.backgroundImage.image.type,             // img type
+                            'background'                                   // img target
+                        );
+                    imagesData.push(backgroundImageData);
+                }
             }
 
             $q.all(imagesData).then(function(images) {
@@ -375,6 +427,53 @@
 
         };
 
+        var initDraggableImages = function() {
+            $('#banner-preview, #background-image').each(function(i, item) {
+                var $imgEl = $(item),
+                    $container = $imgEl.closest('.js-image-container'),
+                    contWidth = $container.width(),
+                    contHeight = $container.height(),
+                    scopeObject = $container.data('scope-object');
+
+                var y1 = contHeight,
+                    x1 = contWidth,
+                    y2 = $imgEl.height(),
+                    x2 = $imgEl.width();
+
+                $imgEl.draggable({
+                    scroll: false,
+                    drag: function(event, ui) {
+                        if(ui.position.top >= 0) {
+                            ui.position.top = 0;
+                        } else if( ui.position.top <= y1 - y2) {
+                            ui.position.top = y1 - y2;
+                        }
+
+                        if( ui.position.left >= 0) {
+                            ui.position.left = 0;
+                        } else if ( ui.position.left <= x1 - x2) {
+                            ui.position.left = x1 - x2;
+                        }
+                    },
+                    stop: function(event, ui) {
+                        $timeout(function() {
+                            if (scopeObject === 'bannerLogo') {
+                                $scope.bannerLogo.image.top = ui.position.top;
+                                $scope.bannerLogo.image.left = ui.position.left;
+                            } else if (scopeObject === 'backgroundImage') {
+                                $scope.backgroundImage.image.top = ui.position.top;
+                                $scope.backgroundImage.image.left = ui.position.left;
+                            }
+                        });
+                    }
+                });
+
+                $scope.saveEditingBgImage();
+                $scope.cancelEditingBgImage();
+                $scope.saveEditingBanner();
+                $scope.cancelEditingBanner();
+            });
+        };
 
         $('.js-upload-banner').on('change', function(evt) {
 
@@ -411,12 +510,9 @@
 
                             if( evt.type === 'load' ){
                                 // Success
-                                // var dataURL = evt.result;
                                 var url = img.toDataURL(file.type);
 
-                                // $imgEl.attr('src', url).addClass('in');
                                 $imgEl.css({top: 0, left: 0});
-                                $container.addClass('image-loaded');
 
                                 // Update angular scope
 
@@ -467,7 +563,7 @@
                                         });
                                     }
                                 });
-                            } else if( evt.type =='progress' ){
+                            } else if (evt.type =='progress'){
                                 var pr = evt.loaded/evt.total * 100;
                             } else {
                                 console.log('error');
@@ -480,11 +576,49 @@
                 }
             });
         });
-    }]).factory('signUpFormService', ['$http', function($http) {
+    }]).factory('signUpFormService', ['$http', '$q', '$location', function($http, $q, $location) {
         var clientSlug = $('#clientSlug').text(),
-            apiClientUrl = '/api/client/'+clientSlug;
+            apiClientUrl = '/api/client/'+clientSlug,
+            formType;
+
+        var urlArr = window.location.pathname.split('/').reverse();
+
+        if (urlArr[0] === 'edit') {
+            formType = 'edit';
+        } else {
+            formType = 'create';
+        }
+
+        // Initial data:
+
+        var defaultData = {};
+        defaultData.formType = formType;
+        defaultData.title = {text: 'some title', isEditing: false};
+        defaultData.permalink = {text: 'some-perma-link', isEditing: false};
+        defaultData.formTitle = {text: 'Header 4 would have max 75 characters', isEditing: false, height: 70};
+        defaultData.formDescription = {text: 'Description would have max 75 characters', isEditing: false, height: 70};
 
         return {
+            getInitialData: function() {
+                var defer = $q.defer();
+
+                var urlArr = window.location.pathname.split('/').reverse();
+
+                if (urlArr[0] === 'edit') {
+                    var formSlug = urlArr[1];
+
+                    $http({
+                        url: apiClientUrl+'/signup_forms/'+formSlug,
+                        method: 'GET'
+                    }).then(function(data) {
+                        defer.resolve(data.data.data);
+                    });
+                } else {
+                    defer.resolve(defaultData);
+                }
+
+                return defer.promise;
+            },
             getClienImages: function() {
                 return $http({
                     url: apiClientUrl+'/images',
@@ -516,6 +650,8 @@
                 });
             },
             uploadFormData: function(data, slug) {
+                var method =
+
                 return $http({
                     url: apiClientUrl+'/signup_forms',
                     method: 'POST',
