e74470fe0223f93ee683226cbc1bcdc509984fa4
Alexander Kulakov <a.kulakov@mail.ru>
2014-01-22 14:01:00 -0400

    Create Event on Probotion being activated or scheduled

--- keen/events/signals.py
+++ keen/events/signals.py
@@ -1,7 +1,15 @@
-from keen.core.models import *
+import logging
+
 from django.db.models.signals import pre_save, post_save, post_delete
 from django.dispatch import receiver
+from django.utils.timezone import now
+
+from keen.core.models import *
 from keen.events.models import *
+
+
+logger = logging.getLogger(__name__)
+
 
 @receiver(post_save, sender=Customer, weak=False)
 def customer_created(sender, instance, created, **kwargs):
@@ -9,11 +17,47 @@ def customer_created(sender, instance, created, **kwargs):
         Event.objects.record_event(client=instance.client, event_type=Event.EVENT_TYPES.cust_new, subject=instance,
                                    occurrence_datetime=instance.created, source=instance.source)
 
+
+@receiver(pre_save, sender=Promotion, weak=False)
+def promotion_status_changed(sender, instance, **kw):
+    if instance.id:
+        try:
+            old_status = Promotion.objects.select_for_update().get(
+                id=instance.id).status
+        except Promotion.DoesNotExist:
+            logger.warn('Failed to find supposedly existing Promotion'
+                        ' with id=%s. Assuming promotion''s status was changed'
+                        % instance.id)
+            old_status = None
+    else:
+        old_status = None
+
+    logger.debug('Checking if ptomotion status was changed from %r to %r'
+                 % (old_status, instance.status))
+
+    if instance.status != old_status:
+        if instance.status == Promotion.PROMOTION_STATUS.active:
+            Event.objects.record_event(
+                client=instance.client,
+                event_type=Event.EVENT_TYPES.promo_active,
+                subject=instance,
+                occurrence_datetime=now(),
+            )
+        elif instance.status == Promotion.PROMOTION_STATUS.scheduled:
+            Event.objects.record_event(
+                client=instance.client,
+                event_type=Event.EVENT_TYPES.promo_scheduled,
+                subject=instance,
+                occurrence_datetime=now(),
+            )
+
+
 @receiver(post_save, sender=Promotion, weak=False)
 def promotion_created(sender, instance, created, **kwargs):
     if created:
         Event.objects.record_event(client=instance.client, event_type=Event.EVENT_TYPES.promo_new, subject=instance,
                                    occurrence_datetime=instance.created)
+
 
 @receiver(post_delete, sender=Promotion, weak=False)
 def promotion_deleted(sender, instance, **kwargs):
