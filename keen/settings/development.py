9c79942948ad4b6d0d3ea8ee2a1b551610698985
Alexander Kulakov <a.kulakov@mail.ru>
2014-01-30 14:13:58 -0400

    Add django-compress and Amazon S3 storage configuration

--- keen/settings/development.py
+++ keen/settings/development.py
@@ -1,7 +1,11 @@
+import datetime
+
 from . import *
 
 
 DEBUG = TEMPLATE_DEBUG = DEV = True
+
+MAILCHIMP_API_KEY = '4f1edbb00b9d47197be82bb7007c1b6d-us7'
 
 INSTALLED_APPS += (
     'debug_toolbar',
@@ -13,4 +17,27 @@ MIDDLEWARE_CLASSES += (
 
 DEBUG_TOOLBAR_PATCH_SETTINGS = False
 
-MAILCHIMP_API_KEY = '4f1edbb00b9d47197be82bb7007c1b6d-us7'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATICFILES_STORAGE = DEFAULT_FILE_STORAGE
+STATIC_URL = 'http://momyc.s3.amazonaws.com/'
+
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_SECURE_URLS = False
+AWS_REDUCED_REDUNDANCY = True
+AWS_STORAGE_BUCKET_NAME = 'momyc'
+AWS_HEADERS = {
+        'Cache-Control': 'max-age=31556926',
+        'Expires': (datetime.datetime.today() + datetime.timedelta(days=365)).strftime('%a, %d %b %Y %H:%M:%S GMT')
+}
+AWS_ACCESS_KEY_ID = 'AKIAI22SZIJA336XJZIQ'
+AWS_SECRET_ACCESS_KEY = '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'
+
+
+COMPRESS_ENABLED = True
+COMPRESS_URL = STATIC_URL
+COMPRESS_STORAGE = DEFAULT_FILE_STORAGE
+COMPRESS_OFFLINE = True
+COMPRESS_CSS_FILTERS = [
+        'compressor.filters.css_default.CssAbsoluteFilter',
+        'compressor.filters.cssmin.CSSMinFilter',
+]
