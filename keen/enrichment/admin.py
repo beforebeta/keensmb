327833bbc9aef7f414dfbf2b23622c31eee9a2ac
Alexander Kulakov <a.kulakov@mail.ru>
2014-01-30 19:46:49 -0400

    Add EnrichemntRequestAdmin

--- keen/enrichment/admin.py
+++ keen/enrichment/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from keen.enrichment.models import EnrichmentRequest
+
+
+class EnrichmentRequestAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(EnrichmentRequest, EnrichmentRequestAdmin)
