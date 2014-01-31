7165be59614510f7ee844e8d12c8f9f4704b47fa
Alexander Kulakov <a.kulakov@mail.ru>
2014-01-31 09:02:19 -0400

    Improve EnrichemntRequest admin

--- keen/enrichment/admin.py
+++ keen/enrichment/admin.py
@@ -4,6 +4,17 @@ from keen.enrichment.models import EnrichmentRequest
 
 
 class EnrichmentRequestAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = ('created', 'client', 'customer_list')
+    exclude = ('modified', 'customers')
+
+    def customer_list(self, req):
+        return '\n'.join(', '.join(filter(None, (
+            (c.data['email'],
+             c.data['full_name'],
+             c.data['dob'],
+             c.data['phone'],
+             c.data['address__zipcode'],
+             )))) for c in req.customers.all())
+
 
 admin.site.register(EnrichmentRequest, EnrichmentRequestAdmin)
