e4d1fb35edba26fd7587e4ac96b2131b1a01a54a
Alexander Kulakov <a.kulakov@mail.ru>
2014-01-31 09:33:35 -0400

    Add links to customers from EnrichmentRequest admin

--- keen/enrichment/admin.py
+++ keen/enrichment/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from django.utils.html import escape
 
 from keen.enrichment.models import EnrichmentRequest
 
@@ -8,13 +10,17 @@ class EnrichmentRequestAdmin(admin.ModelAdmin):
     exclude = ('modified', 'customers')
 
     def customer_list(self, req):
-        return '\n'.join(', '.join(filter(None, (
-            (c.data['email'],
-             c.data['full_name'],
-             c.data['dob'],
-             c.data['phone'],
-             c.data['address__zipcode'],
-             )))) for c in req.customers.all())
+        return mark_safe('\n'.join(self._customer_link(c) for c in req.customers.all()))
+
+    def _customer_link(self, cust):
+        return mark_safe('<a href="{url}">{label}</a>'.format(
+            url='/admin/core/customer/{customer_id}/'.format(customer_id=cust.id),
+            label=escape(', '.join(
+                filter(None, (
+                    cust.data['email'],
+                    cust.data['full_name'],
+                )))),
+        ))
 
 
 admin.site.register(EnrichmentRequest, EnrichmentRequestAdmin)
