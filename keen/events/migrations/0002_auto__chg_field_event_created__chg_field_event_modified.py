47214594a2cdca665c09b4456ae70aaf7c9e95fe
Alexander Kulakov <a.kulakov@mail.ru>
2014-02-03 12:46:05 -0400

    Add migrations for Timestamps descendants

---
+++ keen/events/migrations/0002_auto__chg_field_event_created__chg_field_event_modified.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Event.created'
+        db.alter_column(u'events_event', 'created', self.gf('django.db.models.fields.DateTimeField')())
+
+        # Changing field 'Event.modified'
+        db.alter_column(u'events_event', 'modified', self.gf('django.db.models.fields.DateTimeField')())
+
+    def backwards(self, orm):
+
+        # Changing field 'Event.created'
+        db.alter_column(u'events_event', 'created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True))
+
+        # Changing field 'Event.modified'
+        db.alter_column(u'events_event', 'modified', self.gf('django.db.models.fields.DateTimeField')(auto_now=True))
+
+    models = {
+        u'core.address': {
+            'Meta': {'object_name': 'Address'},
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'country': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'map_image_url': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'postal_code': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'state_province': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'street': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'core.client': {
+            'Meta': {'object_name': 'Client'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'customer_fields': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['core.CustomerField']", 'symmetrical': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'main_location': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'+'", 'null': 'True', 'to': u"orm['core.Location']"}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'}),
+            'ref_id': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'ref_id_type': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'}),
+            'web_url': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'})
+        },
+        u'core.customerfield': {
+            'Meta': {'object_name': 'CustomerField'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'group': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'fields'", 'to': u"orm['core.CustomerFieldGroup']"}),
+            'group_ranking': ('django.db.models.fields.IntegerField', [], {'default': '99999999'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_unique': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '64'}),
+            'required': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'title': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'width': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'})
+        },
+        u'core.customerfieldgroup': {
+            'Meta': {'object_name': 'CustomerFieldGroup'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '64'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'})
+        },
+        u'core.location': {
+            'Meta': {'unique_together': "(('name', 'client'),)", 'object_name': 'Location'},
+            'address': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.Address']"}),
+            'client': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'locations'", 'to': u"orm['core.Client']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'events.event': {
+            'Meta': {'object_name': 'Event'},
+            'client': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.Client']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'data': ('django_hstore.fields.DictionaryField', [], {'db_index': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_aggregate': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'occurrence_datetime': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2014, 2, 3, 0, 0)'})
+        }
+    }
+
+    complete_apps = ['events']
