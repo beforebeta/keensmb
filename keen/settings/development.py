1fc71ff496d3809af4d4e1eec6b69de9244ca6f7
Alexander Kulakov <a.kulakov@mail.ru>
2014-02-04 18:25:36 -0400

    Fix development/production Amazon S3 configuration

--- keen/settings/development.py
+++ keen/settings/development.py
@@ -19,25 +19,25 @@ DEBUG_TOOLBAR_PATCH_SETTINGS = False
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 STATICFILES_STORAGE = DEFAULT_FILE_STORAGE
-STATIC_URL = 'http://momyc.s3.amazonaws.com/'
 
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_SECURE_URLS = False
 AWS_REDUCED_REDUNDANCY = True
 AWS_STORAGE_BUCKET_NAME = 'momyc'
 AWS_HEADERS = {
-        'Cache-Control': 'max-age=31556926',
-        'Expires': (datetime.datetime.today() + datetime.timedelta(days=365)).strftime('%a, %d %b %Y %H:%M:%S GMT')
+    'Cache-Control': 'max-age=31556926',
+    'Expires': (datetime.datetime.today() + datetime.timedelta(days=365)).strftime('%a, %d %b %Y %H:%M:%S GMT')
 }
 AWS_ACCESS_KEY_ID = 'AKIAI22SZIJA336XJZIQ'
 AWS_SECRET_ACCESS_KEY = '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'
 
+STATIC_URL = 'http://{0}.s3.amazonaws.com/'.format(AWS_STORAGE_BUCKET_NAME)
 
 COMPRESS_ENABLED = True
+COMPRESS_OFFLINE = True
 COMPRESS_OUTPUT_DIR = ''
 COMPRESS_URL = STATIC_URL
 COMPRESS_STORAGE = DEFAULT_FILE_STORAGE
-COMPRESS_OFFLINE = True
 COMPRESS_CSS_FILTERS = [
         'compressor.filters.css_default.CssAbsoluteFilter',
         'compressor.filters.cssmin.CSSMinFilter',
