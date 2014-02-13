f33e97240f26403bdce1000bcef0970ee5cd13db
Sergii Iushchuk <edifear@gmail.com>
2014-02-13 20:38:52 +0200

    import csv, first step finished

--- keen/web/static/js/controllers/importCsvCtrl.js
+++ keen/web/static/js/controllers/importCsvCtrl.js
@@ -3,9 +3,16 @@
 'use strict';
 
 angular.module('keen')
-    .controller('importCsvCtrl', ['$scope', '$timeout', '$upload', 'customerListService', function($scope, $timeout, $upload, customerService){
+    .controller('importCsvCtrl', ['$scope', '$timeout', '$upload', '$http', 'customerListService', function($scope, $timeout, $upload, $http, customerService){
         var imScope = this,
             clientSlug = customerService.clientSlug;
+
+        function init() {
+            imScope.isWaiting = false;
+            imScope.fileSelected = {};
+            imScope.activeStep = 1;
+        }
+        init();
 
         $scope.globalAlert = false;
         var notify = function(text) {
@@ -15,54 +22,60 @@ angular.module('keen')
             });
         };
 
-        imScope.importFileChanged = function(e) {
-            console.log(e);
+
+        var selectedFile;
+        imScope.onFileSelect = function($files) {
+            var file = $files[0];
+
+            if (file.type !== 'text/csv') {
+                alert('wrong file type');
+                return false;
+            }
+
+            var fileSize = (999999 >= file.size) ? Math.round(file.size / 1000) + ' KB' : Math.round(file.size / 100000)/10 + ' MB';
+            imScope.fileSelected.size = fileSize;
+            imScope.fileSelected.name = file.name;
+            selectedFile = file;
         };
 
-        imScope.onFileSelect = function($files) {
-            var formData = new FormData($('#import-form')[0]);
-            $.ajax({
-                url: '/api/client/'+clientSlug+'/customers/import',  //Server script to process data
-                type: 'POST',
-                // xhr: function() {  // Custom XMLHttpRequest
-                //     var myXhr = $.ajaxSettings.xhr();
-                //     if(myXhr.upload){ // Check if upload property exists
-                //         myXhr.upload.addEventListener('progress',progressHandlingFunction, false); // For handling the progress of the upload
-                //     }
-                //     return myXhr;
-                // },
-                //Ajax events
-                // beforeSend: beforeSendHandler,
-                success: function(data) {
+        imScope.uploadFile = function() {
+            if (selectedFile) {
+                imScope.isWaiting = true;
+                $upload.upload({
+                    url: '/api/client/'+clientSlug+'/customers/import',
+                    method: 'POST',
+                    // headers: {'headerKey': 'headerValue'},
+                    // withCredential: true,
+                    // data: {myObj: $scope.myModelObj},
+                    file: selectedFile,
+                    // file: $files, //upload multiple files, this feature only works in HTML5 FromData browsers
+                    /* set file formData name for 'Content-Desposition' header. Default: 'file' */
+                    //fileFormDataName: myFile, //OR for HTML5 multiple upload only a list: ['name1', 'name2', ...]
+                    //formDataAppender: function(formData, key, val){} //#40#issuecomment-28612000
+                }).progress(function(evt) {
+                    // console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+                }).success(function(data, status, headers, config) {
                     console.log(data);
-                },
-                // error: errorHandler,
-                // Form data
-                data: formData,
-                //Options to tell jQuery not to process data or worry about content-type.
-                cache: false,
-                contentType: false,
-                processData: false
-            });
 
+                    var reqId = data.import_requiest_id;
+                    imScope.fileSelected.uploaded = true;
 
-            var file = $files[0];
-            $upload.upload({
-                url: '/api/client/'+clientSlug+'/customers/import',
-                method: 'POST',
-                // headers: {'headerKey': 'headerValue'},
-                // withCredential: true,
-                // data: {myObj: $scope.myModelObj},
-                file: file,
-                // file: $files, //upload multiple files, this feature only works in HTML5 FromData browsers
-                /* set file formData name for 'Content-Desposition' header. Default: 'file' */
-                //fileFormDataName: myFile, //OR for HTML5 multiple upload only a list: ['name1', 'name2', ...]
-                //formDataAppender: function(formData, key, val){} //#40#issuecomment-28612000
-            }).progress(function(evt) {
-                console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
-            }).success(function(data, status, headers, config) {
-                console.log(data);
-            });
+                    $timeout(function() {
+                        $http({
+                            method: 'GET',
+                            url: '/api/client/'+ clientSlug +'/customers/import/'+reqId
+                        }).then(function(res) {
+                            imScope.isWaiting = false;
+                            console.log('res: ', res);
+                        });
+                    }, 1000);
+                });
+            }
+        };
+
+        imScope.removeFile = function() {
+            selectedFile = undefined;
+            imScope.fileSelected = {};
         };
         // FileAPI.event.on(el, 'change', function (evt){
         //     var files = FileAPI.getFiles(evt);
