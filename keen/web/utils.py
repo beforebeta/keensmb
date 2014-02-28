53ee47a01f12131accaf30612994e93c4fc86d41
Alexander Kulakov <a.kulakov@mail.ru>
2014-02-28 09:14:08 -0400

    Remove stale code

--- keen/web/utils.py
+++ keen/web/utils.py
@@ -1,15 +1,6 @@
 import csv
 
 
-def row_reader(f):
-    # we cannot use file as source since there migt be CSV files with weired
-    # end-of-line marks
-    for row in f:
-        row = row.strip()
-        print repr(row)
-        yield row
-
-
 def csv_reader(f):
     """Generate sequence of rows from CSV file striping whitespace from each value
     """
