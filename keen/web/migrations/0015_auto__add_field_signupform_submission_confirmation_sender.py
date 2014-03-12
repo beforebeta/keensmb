c0ae3411abc6705c99f7eb88735eedd7afb9a94c
Alexander Kulakov <a.kulakov@mail.ru>
2014-03-12 09:54:12 -0400

    Add migration

---
+++ keen/web/migrations/0015_auto__add_field_signupform_submission_confirmation_sender.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'SignupForm.submission_confirmation_sender'
+        db.add_column(u'web_signupform', 'submission_confirmation_sender',
+                      self.gf('django.db.models.fields.EmailField')(max_length=75, null=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'SignupForm.submission_confirmation_sender'
+        db.delete_column(u'web_signupform', 'submission_confirmation_sender')
+
+
+    models = {
+        u'core.address': {
+            'Meta': {'object_name': 'Address'},
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'country': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'map_image_url': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'postal_code': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'state_province': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'street': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'core.client': {
+            'Meta': {'object_name': 'Client'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'customer_fields': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['core.CustomerField']", 'symmetrical': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'main_location': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'+'", 'null': 'True', 'to': u"orm['core.Location']"}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'}),
+            'ref_id': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'ref_id_type': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'signup_form_code': ('django.db.models.fields.TextField', [], {'default': "''", 'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'}),
+            'web_url': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'})
+        },
+        u'core.customerfield': {
+            'Meta': {'object_name': 'CustomerField'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'group': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'fields'", 'to': u"orm['core.CustomerFieldGroup']"}),
+            'group_ranking': ('django.db.models.fields.IntegerField', [], {'default': '99999999'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_unique': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '64'}),
+            'required': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'title': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'width': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'})
+        },
+        u'core.customerfieldgroup': {
+            'Meta': {'object_name': 'CustomerFieldGroup'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '64'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'})
+        },
+        u'core.location': {
+            'Meta': {'unique_together': "(('name', 'client'),)", 'object_name': 'Location'},
+            'address': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.Address']"}),
+            'client': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'locations'", 'to': u"orm['core.Client']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'tracking.visitor': {
+            'Meta': {'object_name': 'Visitor'},
+            'campaign': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True'}),
+            'content': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'first_visit': ('django.db.models.fields.DateTimeField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ip_address': ('django.db.models.fields.IPAddressField', [], {'max_length': '15'}),
+            'keywords': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True'}),
+            'last_visit': ('django.db.models.fields.DateTimeField', [], {}),
+            'medium': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'referrer': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True'}),
+            'source': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True'}),
+            'term': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True'}),
+            'user_agent': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'visits': ('django.db.models.fields.IntegerField', [], {})
+        },
+        u'web.pagecustomerfield': {
+            'Meta': {'object_name': 'PageCustomerField'},
+            'client': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.Client']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'fields': ('django.db.models.fields.CharField', [], {'default': "'first_name,last_name,email'", 'max_length': '1024'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'page': ('django.db.models.fields.CharField', [], {'max_length': '3'})
+        },
+        u'web.signupform': {
+            'Meta': {'unique_together': "(('client', 'slug'),)", 'object_name': 'SignupForm'},
+            'client': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'signup_forms'", 'to': u"orm['core.Client']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'data': ('jsonfield.fields.JSONField', [], {'default': '{}'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'signup_confirmation_subject': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "'draft'", 'max_length': '32'}),
+            'submission_confirmation_html': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'submission_confirmation_sender': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'null': 'True', 'blank': 'True'}),
+            'submission_notification': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'visitors': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'signup_forms'", 'symmetrical': 'False', 'to': u"orm['tracking.Visitor']"}),
+            'visits': ('django.db.models.fields.IntegerField', [], {'default': '0'})
+        },
+        u'web.trialrequest': {
+            'Meta': {'object_name': 'TrialRequest'},
+            'business': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'comments': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'phone': ('django.db.models.fields.CharField', [], {'max_length': '32', 'null': 'True', 'blank': 'True'}),
+            'question': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'source': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'visitor': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['tracking.Visitor']", 'null': 'True'})
+        }
+    }
+
+    complete_apps = ['web']
